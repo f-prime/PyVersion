019258a82ef47e40169c0d596940c80345d57969
Frankie <frankie@frankie-NV55C.(none)>
2012-12-30 16:19:02 -0500

    First working commit

---
+++ client.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import socket, sys, time
+
+class client:
+    def __init__(self):
+        self.cmd = sys.argv[1]
+        self.file = sys.argv[2]
+        self.ip = sys.argv[3]
+        self.port = int(sys.argv[4]) 
+        self.cmds = {
+
+                "send":self.send,
+                "get":self.get,
+                "del":self.dele,
+                "list":self.list,
+
+                }
+    def main_loop(self):
+        self.sock = socket.socket()
+        self.sock.connect((self.ip, self.port))
+        self.cmds[self.cmd]()
+
+    def list(self):
+        self.sock.send("list")
+        print self.sock.recv(1024)
+        self.sock.close()
+
+    def send(self):
+        try:
+            self.sock.send("send "+self.file+"\n\r\n\r")
+            with open(self.file, 'rb') as file:
+                for line in file.readlines():
+                    self.sock.send(line)
+            self.sock.close()
+        except IOError:
+            print "File does not exist."
+            self.sock.close()
+
+
+
+    def get(self):
+        self.sock.send("get "+self.file)
+        with open(self.file, 'wb') as file:
+            while True:
+                data = self.sock.recv(1024)
+                if not data:
+                    self.sock.close()
+                    break
+                file.write(data)
+
+    def dele(self):
+        self.sock.send("del "+self.file)
+        self.sock.close()
+
+if __name__ == "__main__":
+    try:
+        client().main_loop()
+    except IndexError:
+        print "Usage: python client.py <list|send|get|del> <file name or none is using list command> <ip> <port>"
---
+++ server.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+import socket, os, sys
+
+class server:
+    def __init__(self):
+        self.port = int(sys.argv[1])
+        self.cmds = {
+
+                "send":self.send,
+                "get":self.get,
+                "del":self.dele,
+                "list":self.list,
+
+                }
+    def main_loop(self):
+        sock = socket.socket()
+        sock.bind(('', self.port))
+        sock.listen(5)
+        while True:
+            self.obj, conn = sock.accept()
+            print conn[0]
+            data = self.obj.recv(1024)
+            self.data = data
+            try:
+                self.file = data.split()[1]
+            except:
+                pass
+
+            self.cmds[data.split()[0]]()
+
+    def send(self):
+        with open(self.file, 'wb') as file:
+            while True:
+                data = self.obj.recv(1024)
+                if not data:
+                    self.obj.close()
+                    break
+                file.write(data)
+
+    def list(self):
+        self.obj.send(str(os.listdir(os.getcwd())))
+        self.obj.close()
+    def get(self):
+        try:
+            file = self.data.split()[1]
+            with open(file, 'rb') as file:
+                for line in file.readlines():
+                    self.obj.send(line)
+            self.obj.close()
+        except IOError:
+            self.obj.send("File does not exist")
+            self.obj.close()
+
+    def dele(self):
+        try:
+            file = self.data.split()[1]
+            os.remove(file)
+            self.obj.close()
+        except OSError:
+            self.obj.send("File does not exist.")
+            self.obj.close()
+
+if __name__ == "__main__":
+    try:
+        server().main_loop()
+    except IndexError:
+        print "Usage: python server.py <port>"
+
